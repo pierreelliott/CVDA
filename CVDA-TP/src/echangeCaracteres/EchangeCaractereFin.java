ac8303b3c65e1cd1a1e432e374f875afa17752fe
unknown <p1402690@IUTDOUAB2PC73.univ-lyon1.fr>
2016-06-20 14:03:33 +0200

    Crea TP

---
+++ CVDA-TP/src/echangeCaracteres/EchangeCaractereFin.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package echangeCaracteres;
+
+/**
+ *
+ * @author p1402690
+ */
+public class EchangeCaractereFin {
+    
+    
+    public static String echange2DernierCar(String s){
+        
+        if(s.length()>1){
+            if(s.length()>2){
+                return s.substring(0, s.length()-2)+s.charAt(s.length()-1)+s.charAt(s.length()-2);}
+            return ""+s.charAt(s.length()-1)+s.charAt(s.length()-2);
+        }
+        else return s;
+    }
+    
+    
+}
